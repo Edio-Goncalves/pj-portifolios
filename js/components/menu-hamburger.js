3cde6cc31051befc1ece305e74f58430cf56116d
Edio Goncalves <ediogoncalvesjr@hotmail.com>
2022-12-24 20:27:43 -0300

    Refactory the header in index html

---
+++ js/components/menu-hamburger.js
@@ -0,0 +1,7 @@
+const menuMobile = document.getElementById("menu-mobile");
+
+function toggleMenu() {
+  const active = document.getElementById("menu-mobile");
+  active.classList.toggle("active");
+}
+menuMobile.addEventListener("click", toggleMenu);
